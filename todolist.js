df87e6d87523ddf40ce3ad714fd0284f53c1b563
nam241806798 <hoangvannam17031998@gmail.com>
2022-04-02 14:20:48 +0700

    gix

---
+++ todolist.js
@@ -0,0 +1,13 @@
+function todolist() {
+	var list = document.getElementById("input__value").value;
+	var t = document.createTextNode(list);
+	li.appendChild(t);
+	
+
+
+}
+function car() {
+	console.log(12)
+}
+	
+ 
